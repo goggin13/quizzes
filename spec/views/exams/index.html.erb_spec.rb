78ee15835adeaef73b7b56bba49c9ea06e554b39
goggin13 <goggin13@gmail.com>
2021-01-27 20:32:47 -0600

    add nested links

--- spec/views/exams/index.html.erb_spec.rb
+++ spec/views/exams/index.html.erb_spec.rb
@@ -2,21 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "exams/index", type: :view do
   before(:each) do
-    assign(:exams, [
-      Exam.create!(
-        title: "Title",
-        open: false
-      ),
-      Exam.create!(
-        title: "Title",
-        open: false
-      )
-    ])
+    @exams = (0..1).map { FactoryBot.create(:exam) }
+    assign(:exams, @exams)
   end
 
   it "renders a list of exams" do
     render
-    assert_select "tr>td", text: "Title".to_s, count: 2
+    assert_select "tr>td", text: "MyString".to_s, count: 2
     assert_select "tr>td", text: false.to_s, count: 2
+    expect(rendered).to match("href=\"/exams/#{@exams[0].id}/questions\"")
+    expect(rendered).to match("href=\"/exams/#{@exams[1].id}/questions\"")
   end
 end
--- spec/views/questions/index.html.erb_spec.rb
+++ spec/views/questions/index.html.erb_spec.rb
@@ -3,10 +3,11 @@ require 'rails_helper'
 RSpec.describe "questions/index", type: :view do
   before(:each) do
     @exam = FactoryBot.create(:exam)
-    assign(:questions, [
+    @questions = [
       FactoryBot.create(:question, exam: @exam),
       FactoryBot.create(:question, exam: @exam)
-    ])
+    ]
+    assign(:questions, @questions)
   end
 
   it "renders a list of questions" do
@@ -14,5 +15,7 @@ RSpec.describe "questions/index", type: :view do
     assert_select "tr>td", text: "Prompt".to_s, count: 2
     assert_select "tr>td", text: "MyString".to_s, count: 2
     assert_select "tr>td", text: "MyText".to_s, count: 2
+    expect(rendered).to match("href=\"/questions/#{@questions[0].id}/answers\"")
+    expect(rendered).to match("href=\"/questions/#{@questions[1].id}/answers\"")
   end
 end
