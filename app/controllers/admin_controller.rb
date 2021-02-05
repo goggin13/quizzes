a1ef536e777e93c9692f5748d961decbd2b7b133
goggin13 <goggin13@gmail.com>
2021-02-04 18:54:21 -0600

    Simple admin view

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,9 @@
+class AdminController < ApplicationController
+  def index
+    question_ids = UserAnswer.all.map(&:question_id)
+    @answered_question_counts = question_ids
+      .group_by{ |i| i }
+      .map{ |k,v| [k, v.count] }
+      .reject { |k,v| v < 2 }
+  end
+end
